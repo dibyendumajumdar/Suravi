9832bdff517e428b70e611ea9e7c1329cfb635de
Dibyendu Majumdar <noemail>
2018-05-06 21:46:17 +0100

    add build script

---
+++ build/buildmsvc_lua53.bat
@@ -0,0 +1,119 @@
+cd lua
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-ffi
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch-paths
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch7-sys
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch7
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch7-xlua
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-moses
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-filesystem
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch7-nn
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch7-optim
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-torch-cephes
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-penlight
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-cjson
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-lpeglabel
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..
+
+cd ravi-luasocket
+rmdir build /s
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=c:/Software/lua53 -DUSE_LUA53=ON -G "Visual Studio 15 2017 Win64" ..
+cmake --build . --config Release --target INSTALL
+cd ..\..

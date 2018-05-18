8b8a030037ee0064cb79b56ad2b3c8a6eee48d18
dibyendumajumdar <noemail>
2018-05-18 23:54:49 +0100

    add luv to the Linux build

--- build/linux_build.sh
+++ build/linux_build.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH="$RAVI_HOME/lib:$LD_LIBRARY_PATH"
 export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
 export LUA_CPATH="$RAVI_HOME/lib/?.so;$RAVI_HOME/lib/lib?.so"
 
-for dir in ravi-external-libs ravi ravi-cjson ravi-ffi ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes; do
+for dir in ravi-external-libs ravi ravi-cjson ravi-ffi ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv; do
 	echo "Building $dir"
 	cd $dir && rm -rf build && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
 done

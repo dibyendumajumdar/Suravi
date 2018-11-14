bfdb5248d7eafe5bd5771a27ebbbed58bc8178d5
dibyendumajumdar <noemail>
2018-11-14 11:26:32 +0000

    enable OMR JIT on Linux

--- build/linux_build.sh
+++ build/linux_build.sh
@@ -6,5 +6,5 @@ export LUA_CPATH="$RAVI_HOME/lib/?.so;$RAVI_HOME/lib/lib?.so"
 
 for dir in nj ravi-external-libs ravi ravi-cjson ravi-ffi ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv ravi-protobuf; do
 	echo "Building $dir"
-	cd $dir && rm -rf build && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
+	cd $dir && rm -rf build && mkdir build && cd build && cmake -DOMR_JIT=ON -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
 done

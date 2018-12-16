5cf52cba31f6e645b164810336bc830cc6b20b39
Dibyendu Majumdar <noemail>
2018-12-16 23:33:48 +0000

    On Mac OSX we need to set PATH to find openssl library installed by brew

--- build/osx_build.sh
+++ build/osx_build.sh
@@ -4,6 +4,8 @@ export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
 export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
 export LUA_CPATH="$RAVI_HOME/lib/?.dylib;$RAVI_HOME/lib/lib?.dylib"
 export LUA_CPATH="$LUA_CPATH;$RAVI_HOME/lib/?.so;$RAVI_HOME/lib/lib?.so"
+# Note following assumes brew install openssl
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
 
 for dir in nj ravi-external-libs ravi ravi-cjson ravi-ffi ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv ravi-protobuf ravi-luaossl; do
 	echo "Building $dir"

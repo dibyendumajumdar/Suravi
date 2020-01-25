4a87f72af5bdcc67af499a264ac42bc1f4c33eac
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2020-01-25 23:37:45 +0000

    Update osx_build.sh

--- build/osx_build.sh
+++ build/osx_build.sh
@@ -1,4 +1,11 @@
-export RAVI_HOME=$HOME/ravi
+#!/bin/sh
+
+if [ "$#" -ne 1 ] ; then
+    echo "Supply the installation path"
+	exit 1
+fi
+
+export RAVI_HOME=$1
 export PATH=$RAVI_HOME/bin:$PATH
 export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
 export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
@@ -9,5 +16,5 @@ export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
 
 for dir in ravi ravi-cjson ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv ravi-protobuf ravi-luaossl; do
 	echo "Building $dir"
-	cd $dir && rm -rf build && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
+	cd $dir && rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
 done

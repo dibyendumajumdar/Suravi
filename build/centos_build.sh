d8f072fc418fe2520660a4fa88b5b932a50f3cb4
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2023-01-13 21:13:13 +0000

    Update centos_build.sh

--- build/centos_build.sh
+++ build/centos_build.sh
@@ -11,7 +11,7 @@ export LD_LIBRARY_PATH="$RAVI_HOME/lib64:$LD_LIBRARY_PATH"
 export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
 export LUA_CPATH="$RAVI_HOME/lib64/?.so;$RAVI_HOME/lib64/lib?.so"
 
-for dir in ravi ravi-cjson ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-simplex ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv ravi-protobuf ravi-luaossl; do
+for dir in ravi ravi-cjson ravi-filesystem ravi-lpeglabel ravi-luasocket ravi-moses ravi-penlight ravi-simplex ravi-torch-paths ravi-torch7-sys ravi-torch7 ravi-torch7-xlua ravi-torch7-nn ravi-torch7-optim ravi-torch7-autograd ravi-torch-cephes ravi-libuv-luv ravi-protobuf; do
 	echo "Building $dir"
 	cd $dir && rm -rf build && mkdir build && cd build && cmake3 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$RAVI_HOME .. && make install && cd ../..
 done
